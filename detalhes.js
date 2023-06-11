4f98f1fd26b64f9c623f85a3333a02983beb3ade
c44ac <129755617+c44ac@users.noreply.github.com>
2023-06-11 20:46:26 -0300

    Add files via upload

---
+++ detalhes.js
@@ -0,0 +1,73 @@
+// pegando id da url
+var url = window.location.href;
+var id = url.match(/id=(\d+)/)[1];
+
+console.log(url);
+console.log(id);
+
+// inserido elementos da api na tela
+function inserindoElementos(imagemUrl, titulo, descricao, preco, ano, avaliacao, totalAv, tipo, gender) {
+    const secao1 = document.getElementById('secao1');
+    const imagem = document.createElement('img');
+    imagem.src = imagemUrl;
+    imagem.classList.add('col-8');
+    secao1.appendChild(imagem);
+
+    const secao2 = document.getElementById("secao2");
+    const elementoTitulo = document.createElement("h1");
+    const elementoDescricao = document.createElement("p");
+    const elementoPreco = document.createElement("p");
+    const elementoAno = document.createElement("p");
+    const elementoAvaliacao = document.createElement("p");
+    const elementototalAvaliacoes = document.createElement("p");
+    const elementotipo = document.createElement("p");
+    const elementogender = document.createElement("p");
+
+    elementoTitulo.textContent = titulo;
+    elementoDescricao.innerHTML = descricao;
+    elementoPreco.textContent = "Preço: R$ " + preco;
+    elementoAno.textContent = "Ano de lançamento: " + ano;
+    elementoAvaliacao.textContent = "Nota de avaliação: " + avaliacao;
+    elementototalAvaliacoes.textContent = "Total de avaliações: " + totalAv;
+    elementotipo.textContent = "Tipo/categoria do produto: " + tipo;
+    elementogender.textContent = "Gênero para o qual o produto é destinado: " + gender;
+
+    secao2.appendChild(elementoTitulo);
+    secao2.appendChild(elementoDescricao);
+    secao2.appendChild(elementoPreco);
+    secao2.appendChild(elementoAno);
+    secao2.appendChild(elementoAvaliacao);
+    secao2.appendChild(elementototalAvaliacoes);
+    secao2.appendChild(elementotipo);
+    secao2.appendChild(elementogender);
+}
+
+// chamando api
+async function carregarConteudo() {
+    const apiUrl = "https://diwserver.vps.webdock.cloud/products/" + id;
+    console.log(apiUrl);
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const imagemUrl = data.image;
+        const titulo = data.title;
+        const descricao = data.description;
+        const preco = data.price;
+        const ano = data.year;
+        const avaliacao = data.rating.rate;
+        const totalAv = data.rating.count;
+        const tipo = data.articleType;
+        const gender = data.gender;
+
+        inserindoElementos(imagemUrl, titulo, descricao, preco, ano, avaliacao, totalAv, tipo, gender);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+carregarConteudo();
+
+
+
---
+++ script.js
@@ -0,0 +1,141 @@
+let products = [];
+const DOM = {
+    productsContainer: document.querySelector('#products-list'),
+
+    addProduct(product, index) {
+        const element = document.createElement('div')
+        element.classList.add("col-md-6")
+        element.classList.add("col-lg-4")
+        element.innerHTML = DOM.innerHTMLProduct(product, index)
+        element.dataset.index = index
+
+        DOM.productsContainer.appendChild(element)
+    },
+
+    innerHTMLProduct(product, index) {
+        return `
+            <div class="product card mb-2 m-2">
+                    <img src=${product.image} class="card-img-top"
+                        alt="Imagem produto">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text fw-bold">R$ ${product.price}</p>
+                        <p class="card-text">${product.description}</p>
+                        <p class="card-text">Avaliação: ${product.rating.rate}</p>
+                        <p class="card-text">Ano de lançamento: ${product.year}</p>
+                        <a href="detalhes.html?id=${product.id}" data-product-id="${product.id}" target="_blank" class="btn btn-primary">Mais detalhes</a>
+                    </div>
+            </div>
+        `
+    }
+}
+const productsAside = [];
+const DOMaside = {
+    productsAsideContainer: document.querySelector('#aside-list'),
+
+
+    addProduct(productAside, index) {
+
+        const element = document.createElement('div')
+        element.classList.add("row")
+        element.innerHTML = DOMaside.innerHTMLProduct(productAside, index)
+        element.dataset.index = index
+        DOMaside.productsAsideContainer.appendChild(element)
+
+    },
+    innerHTMLProduct(productAside) {
+
+        return `
+                    <div class="col-4">
+                        <img src="${productAside.image}" class="img-fluid" alt="Produto">
+                    </div>
+                    <div class="col-8">
+                      <div class="card-body">
+                            <h5 class="card-title">${productAside.title}</h5>
+                            <p class="card-text fw-bold">Avaliação: ${productAside.rating.rate}</p>
+                            <a href="detalhes.html?id=${productAside.id}" data-product-id="${productAside.id}" target="_blank" class="btn btn-primary">Visualizar</a>
+                        </div>
+                    </div>
+                `
+    }
+}
+ async function init(pageNumber) {
+    products.length = 0;
+    productsAside.length = 0;
+
+    await fetch(`https://diwserver.vps.webdock.cloud/products/category/Apparel - Topwear?page=${pageNumber}`)
+        .then(async function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            products.push(...response.products);
+            productsAside.push(...response.products);
+        })
+        .catch(function (error) {
+            console.log("Error: " + error);
+        });
+
+    DOM.productsContainer.innerHTML = "";
+    DOMaside.productsAsideContainer.innerHTML = "";
+
+    products.forEach(product => DOM.addProduct(product));
+    productsAside.sort(function (a, b) {
+        if (a.rating.rate < b.rating.rate) {
+            return 1;
+        }
+        if (a.rating.rate > b.rating.rate) {
+            return -1;
+        }
+        return 0;
+    });
+    for (let i = 0; i < 5; i++) {
+        DOMaside.addProduct(productsAside[i]);
+    }
+} 
+
+init(1)
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const productslist = document.getElementById("products-list");
+    const product = document.querySelectorAll('.product');
+    const productname = productslist.getElementsByTagName("h5");
+
+    for (let i=0; i < products.length; i++) {
+        for (let j = 0; j < productname.length; j++) {
+            let combina = product[j].getElementsByTagName('h5')[0];
+            if (combina) {
+                let textvalue = combina.textContent || combina.innerHTML;
+                if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                    product[j].style.display = "";
+                } else {
+                    product[j].style.display = "none";
+                }
+            }
+        }
+    }
+}
+function searchForm() { 
+    let tipoProduto = document.getElementById("tipo-produto").value;
+    let marcaProduto = document.getElementById("marca-produto").value;
+    let generoProduto = document.getElementById("gender-produto").value;
+    let avaliacaoProduto = document.getElementById("avaliacao-produto").value;
+    let link = document.getElementById("btnProcurar-produto");
+    link.href = `pesquisa.html?`;
+    let params = [];
+
+    if (tipoProduto !== "") {
+        params.push("tipo=" + encodeURIComponent(tipoProduto))
+    }
+    if (marcaProduto !== "") {
+        params.push("marca=" + encodeURIComponent(marcaProduto))
+    }
+    if (generoProduto !== "") {
+        params.push("genero=" + encodeURIComponent(generoProduto))
+    }
+    if (avaliacaoProduto !== "") {
+        params.push("avaliacao=" + encodeURIComponent(avaliacaoProduto))
+    }
+    if (params.length > 0) {
+        link.href += params.join("&");
+    }
+}
